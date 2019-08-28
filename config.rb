6cb8d5ba51da6ae12a3d537158225edabd684a9f
Farson <waldemar@gribele.de>
2019-08-28 22:52:07 +0200

    chore: update config

--- config.rb
+++ config.rb
@@ -3,9 +3,13 @@
 :sensor_2: "28-01131621351d"
 :sensor_3: "28-01131797f1e3"
 :heater_1:
-  :pin: 1
+  :pin: 0
   :target_temp: 28
-:heater_2: 30
-:heater_3: 25
+:heater_2:
+  :pin: 2
+  :target_temp: 30
+:heater_3:
+  :pin: 3
+  :target_temp: 25
 :day_start: 7
 :day_end: 19
